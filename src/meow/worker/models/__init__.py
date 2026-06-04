2909e9ea5c79c785f4947569a7b5f74d55310734
Clement PARPAILLON <clem.parpaillon@gmail.com>
2026-06-05 00:21:51 +0200

    refactor(worker): drop obsolete modules from pre-refactor layout
    intent.py, types.py, sandbox/diff_filter.py, models/review_report.py,
    and the old vibe_tasks/pr_review.py are all dead since the workflow
    pipeline was rewired around VibeTask/VibeResult and the
    vibe_tasks/tasks/ package. ReviewReport leaves the models __init__ too.

--- src/meow/worker/models/__init__.py
+++ src/meow/worker/models/__init__.py
@@ -1,7 +1,6 @@
 from meow.worker.models.meow_config import MeowConfig
 from meow.worker.models.pr_context import PrContext
 from meow.worker.models.pr_sandbox_spec import PrSandboxSpec
-from meow.worker.models.review_report import ReviewReport
 from meow.worker.models.vibe_result import VibeResult
 from meow.worker.models.vibe_task import VibeTask
 
@@ -9,7 +8,6 @@ __all__ = [
     "MeowConfig",
     "PrContext",
     "PrSandboxSpec",
-    "ReviewReport",
     "VibeResult",
     "VibeTask",
 ]
